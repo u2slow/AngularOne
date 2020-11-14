ebe3c956f8518b739bab698f798b40101565c4a3
jonas <jonasbullwinkel8@gmail.com>
2020-11-14 12:29:53 +0100

    tetris grundstrucktur

--- src/app/tetris/tetris.component.ts
+++ src/app/tetris/tetris.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TetrisComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
+  }
+
+  ngOnInit(): void {}
+
+  startGame(){
+    document.getElementById('start').remove();
+    const main = document.getElementById('raster');
+    for (let i = 1; i < 9; i++){
+      for (let n = 1; n < 9; n++){
+        const div = document.createElement('div');
+        div.style.gridColumn = n.toString();
+        div.style.gridRow = i.toString();
+        div.id = i + '/' + n;
+        div.className = 'rasterElement';
+        main?.appendChild(div);
+      }
+    }
   }
 
 }
